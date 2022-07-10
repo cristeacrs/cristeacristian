a36164007801020b436006f2ea7d5cb491bcd394
cristeacrs <cristea.cristi@yahoo.com>
2022-07-11 00:29:19 +0300

    [FEAT]: Update skills projects logo.
    Co-authored-by: George Crisan <georgerdp@gmail.com>

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ const startWorkingTogether = () => {
 };
 
 startWorkingTogether();
+
+/* Demo purposes only */
+/* Demo purposes only */
+$(".hover").mouseleave(function () {
+  $(this).removeClass("hover");
+});
